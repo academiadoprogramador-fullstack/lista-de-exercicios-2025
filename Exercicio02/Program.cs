049a9000d7e00c63a93b3b65dbf0aaf165f961c0
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-20 14:13:54 -0300

    Implementado exercício 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        // Crie um programa para calcular o volume de um Cilindro
+        // V = π . r² . h
+        static void Main(string[] args)
+        {
+            double raio = 3.1;
+            double altura = 10;
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindro é: {volume} cm³.");
+
+            Console.ReadLine();
+        }
+    }
+}
